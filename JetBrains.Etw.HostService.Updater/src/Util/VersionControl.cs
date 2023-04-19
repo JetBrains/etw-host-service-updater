85b5476a4656254f8be8d7b4ef57612392259b4f
Mikhail Pilin <mikhail@pilin.name>
2023-04-19 18:15:46 +0200

    Update JetBrains.Microsoft.Deployment.WindowsInstaller to WixToolset.Dtf.WindowsInstaller.

--- JetBrains.Etw.HostService.Updater/src/Util/VersionControl.cs
+++ JetBrains.Etw.HostService.Updater/src/Util/VersionControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using Microsoft.Deployment.WindowsInstaller;
+using WixToolset.Dtf.WindowsInstaller;
 
 namespace JetBrains.Etw.HostService.Updater.Util
 {
